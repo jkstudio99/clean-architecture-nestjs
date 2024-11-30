f7d6cd2531966b3b39b127005a732e619e1a4bf1
jkstudio99 <143254106+jkstudio99@users.noreply.github.com>
2024-11-30 19:55:08 +0700

    add orders swaggers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './interface/http/product.controller';
+import { OrderController } from './interface/http/order.controller';
 import { ProductService } from './application/services/product.service';
+import { OrderService } from './application/services/order.service';
 import { PrismaProductRepository } from './infrastructure/repositories/prisma-product.repository';
+import { PrismaOrderRepository } from './infrastructure/repositories/prisma-order.repository';
 import { PrismaService } from './infrastructure/prisma/prisma.service';
 
 @Module({
-  controllers: [ProductController],
+  controllers: [ProductController, OrderController],
   providers: [
     ProductService,
+    OrderService,
     PrismaService,
     {
       provide: 'ProductRepository',
       useClass: PrismaProductRepository,
+    },
+    {
+      provide: 'OrderRepository',
+      useClass: PrismaOrderRepository,
     },
   ],
 })
